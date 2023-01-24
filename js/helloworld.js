2193de59843e3ed28376074693c50843b460175f
ariztocles <plukarizpinera@gmail.com>
2023-01-25 01:36:49 +0800

    1
    1

--- js/helloworld.js
+++ js/helloworld.js
@@ -11,4 +11,4 @@ console.log("\u25CF Practice and Patience");
 console.log("\u25CF Go Beyond Good Enough");
 console.log("~");
 
-document.write("\"Hello Refocus !!\"<br><br>\"I am gonna be an awesome web developer.\"<br><br>~<br><br>\"PLAY to your STRENGHTS!\"<br><br>\u25CF Know your Aptitude<br><br>\u25CF Recognize Patterns of Failure<br><br>\u25CF Optimise your Operation;<br><br>\u25CF Practice and Patience<br><br>\u25CF Go Beyond Good Enough<br>");
+document.write("\"Hello Refocus !!\"<br><br>\"I am gonna be an awesome web developer.\"<br><br>~<br><br>\"PLAY to your STRENGHTS!\"<br><br>\u25CF Know your Aptitude<br><br>\u25CF Recognize Patterns of Failure<br><br>\u25CF Optimise your Operation;<br><br>\u25CF Practice and Patience<br><br>\u25CF Go Beyond Good Enough<br><br>~<br><br>");
